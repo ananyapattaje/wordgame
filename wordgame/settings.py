8ab9e8f8b739b1edb66b884dd52dd1002d932f55
ananyapattaje <ananyakalluraya@gamil.com>
2025-09-29 21:17:15 +0530

    modified:   game/templates/registration/login.html modified:   game/templates/registration/register.html modified:   wordgame/settings.py

--- wordgame/settings.py
+++ wordgame/settings.py
@@ -92,6 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 5}, # Set minimum length to 5
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
